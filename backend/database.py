9bb96ed749e980522f4cb85d50111cfbc141550c
tulasivaindamfed9 <tulasivaindamfed9@gmail.com>
2026-07-21 12:46:47 +0530

    added requirements file and changed database.py file for db connection

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
 
 # postgre db coonection
-db_url="postgresql://postgres:Postgres12@localhost:5432/TO_DO_List_FAPI"
+db_url= os.getenv("DATABASE_URL")
 # creating an engine to build connection between postgre and our localsession
 engine=create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine) 
